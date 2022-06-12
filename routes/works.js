948dd4d6a4a46ea1c11762880b805056fde68e72
swenlii <swenliw@gmail.com>
2022-06-12 12:11:26 +0300

    check site with lighthouse and change text
    - change all text and labels and translate to en
    - add license
    - change readme
    - check site with lighthouse
    - and change styles
    - all images png --> webp
    - add new work: learn_to_child
    - change favicon
    - little changes in style of visitka

--- routes/works.js
+++ routes/works.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/visitka', function(req, res, next) {
-  res.render('works/visitka/index', { title: 'Swenlii\'s site' });
+  res.render('works/visitka/index', { title: 'One page site' });
+});
+
+router.get('/astro-for-children', function(req, res, next) {
+  res.render('works/astro-for-children/index', { title: 'Astrochild' });
 });
 
 module.exports = router;
