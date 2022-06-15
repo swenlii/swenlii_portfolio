0e79467b030083aa3a0063e4ff95e7ad8f05ae75
swenlii <swenliw@gmail.com>
2022-06-15 13:44:18 +0300

    constants in production

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 let nodemailer = require('nodemailer');
-const constants = require('./CONSTANTS');
+
+if (!process.env.trans_api) {
+  console.log('add to process.env');
+  require('dotenv').config();
+  console.log(process.env);
+  console.log(process.env.TRANS_API)
+}
 
 const app = express();
 
@@ -16,13 +22,14 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 let languageTranslator = require('language-translator');
+console.log(process.env)
 app.use(languageTranslator.init(
     {
       langs          : ["en", "ru"], // ... And other languages
       defaultLang    : "en",
       cookieName     : "lang",
       translate      : "true",
-      translationApiKey: "trnsl.1.1.20191211T150132Z.1202d3e8ec0a8df5.7fd6ce84438b8b8a77950ad90aa716c766f81a7c"
+      translationApiKey: process.env.TRANS_API
     }));
 
 let main_router = require('./routes/index');
@@ -38,18 +45,18 @@ app.post("/send", async function (request, response) {
     body += data;
     let t = JSON.parse(body);
     let transporter = createTransport({
-      host: constants.mail_host,
-      port: constants.mail_port,
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
       secure: true,
       auth: {
-        user: constants.mail_user,
-        pass: constants.mail_pass
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
       }
     });
     await transporter.sendMail({
-      from: constants.mail_user,
+      from: process.env.MAIL_USER,
       sender: decodeURIComponent(t.email),
-      to: constants.mail_user,
+      to: process.env.MAIL_USER,
       subject: decodeURIComponent(t.title),
       html: "<p>Письмо направилено с формы контакта в портфолио</p><h1 style='color: #f80b0b'>" + decodeURIComponent(t.title) + "</h1><p><h3 style='color: #3f6efd'>Name sender:</h3>" + decodeURIComponent(t.name) + "</p><p><h3 style='color: #0b99f8'>Object:</h3>" + decodeURIComponent(t.object) + "</p><p><h3 style='color: #743ffd'>Email for answer:</h3>" + decodeURIComponent(t.email) + "</p><p><h3 style='color: #0bf889'>Text:</h3><pre>" + decodeURIComponent(t.mail) + "</pre></p>",
     }, (error, infoSuccess) => {
